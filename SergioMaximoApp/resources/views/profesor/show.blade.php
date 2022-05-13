96d7f8001dd79dc6d8f0cd260778dbd7d3281bc1
Sergio <sergiogallego676@gmail.com>
2022-05-13 17:31:10 +0200

    permisos editar usuarios sgallego

--- SergioMaximoApp/resources/views/profesor/show.blade.php
+++ SergioMaximoApp/resources/views/profesor/show.blade.php
@@ -8,7 +8,7 @@
         <div class="botones p-3" style="border-width: 1px; background-color: #ffe6cf">
             <x-jet-validation-errors class="mb-4" style="color: red"/>
                 <h3 style="text-align: center">Datos de usuario</h1>
-                <form class="mt-4" method="POST" action="#">
+                <form class="mt-4" method="POST" action="{{ route('updateUser', $profesor->id) }}">
                     
                     @csrf
                     @method('PUT')
@@ -21,14 +21,14 @@
                         <input @if(auth()->user()->id != $profesor->id && auth()->user()->role_id == 2 || (auth()->user()->role_id == 1 && $profesor->role_id == 1 && auth()->user()->id != 1 && auth()->user()->id != $profesor->id)) readonly @endif value="{{$profesor->email}}" placeholder="Correo electrónico" id="email" class="block mt-1 w-full form-control" type="email" name="email" required />
                     </div><br>
                         <div>
-                            <select @if(auth()->user()->id != $profesor->id && auth()->user()->role_id == 2 || (auth()->user()->role_id == 1 && $profesor->role_id == 1 && auth()->user()->id != 1 && auth()->user()->id != $profesor->id || auth()->user()->id == 1 && $profesor->id == 1)) disabled @endif value="{{$profesor->name}}" name="role_id" x-model="role_id" class="block mt-1 w-full form-control border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
-                                <option value="1">Superusuario</option>
-                                <option value="2">Profesor</option>
+                            <select @if(auth()->user()->role_id == 2 || (auth()->user()->role_id == 1 && $profesor->role_id == 1 && auth()->user()->id != 1 && auth()->user()->id != $profesor->id) || auth()->user()->id == 1 && $profesor->id == 1) disabled @endif value="{{$profesor->name}}" name="role_id" x-model="role_id" class="block mt-1 w-full form-control border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                                <option @if($profesor->role_id == 1) selected @endif value="1">Superusuario</option>
+                                <option @if($profesor->role_id == 2) selected @endif value="2">Profesor</option>
                             </select>
                         </div><br>
 
                         <div>
-                            <select @if(auth()->user()->id != $profesor->id && auth()->user()->role_id == 2 || (auth()->user()->role_id == 1 && $profesor->role_id == 1 && auth()->user()->id != 1 && auth()->user()->id != $profesor->id || auth()->user()->id == 1 && $profesor->id == 1)) disabled @endif value="{{$profesor->email}}" value="{{$profesor->name}}" name="estado" x-model="estado" class="block mt-1  w-full form-control border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                            <select @if(auth()->user()->role_id == 2 || (auth()->user()->role_id == 1 && $profesor->role_id == 1 && auth()->user()->id != 1 && auth()->user()->id != $profesor->id) || auth()->user()->id == 1 && $profesor->id == 1) disabled @endif value="{{$profesor->email}}" value="{{$profesor->name}}" name="estado" x-model="estado" class="block mt-1  w-full form-control border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
                                 <option @if($profesor->estado == "Activo") selected @endif value="Activo">Activo</option>
                                 <option @if($profesor->estado == "Inactivo") selected @endif value="Inactivo">Inactivo</option>
                             </select>
