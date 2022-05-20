db37b15bf3a6489cfc81f3026af61e8c8394fa11
Sergio <sergiogallego676@gmail.com>
2022-05-20 16:05:55 +0200

    env sgallego

--- SergioMaximoApp/resources/views/ciclo/stats.blade.php
+++ SergioMaximoApp/resources/views/ciclo/stats.blade.php
@@ -30,39 +30,10 @@
                     <table border="1" style="width: 100%">      
                         <tr>
                             <td class="cabecera"><b>Alumno</b></td>
-                            @foreach($m->ufs as $key => $u)
+                            @foreach($arrayUfs as $u)
                                 <td class="cabecera"><b>{{$u->nombre}} [{{$u->horas}} horas]</b></td>
                             @endforeach
                         </tr>
-                        @foreach($arrayAlumnos as $key => $a)
-                            @if($modulo->ciclos->nombre == $a->ciclo)
-                                <tr>
-                                    <td>{{$a->nombre}} {{$a->apellidos}}</td>
-                                    @foreach($a->ufs as $u)
-                                        @if($u->modulo_id == $modulo->id)
-                                            <td> 
-                                                @if(auth()->user()->role_id == 2)
-                                                    <select name="notas[]" class="col-md-12" style="text-align: center">{{$u->pivot->nota}}
-                                                        <option value="{{$a->id . "_" . $u->id . "_1"}}" @if ($u->pivot->nota == 1) selected @endif>1</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_2"}}" @if ($u->pivot->nota == 2) selected @endif>2</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_3"}}" @if ($u->pivot->nota == 3) selected @endif>3</option>        
-                                                        <option value="{{$a->id . "_" . $u->id . "_4"}}" @if ($u->pivot->nota == 4) selected @endif>4</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_5"}}" @if ($u->pivot->nota == 5) selected @endif>5</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_6"}}" @if ($u->pivot->nota == 6) selected @endif>6</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_7"}}" @if ($u->pivot->nota == 7) selected @endif>7</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_8"}}" @if ($u->pivot->nota == 8) selected @endif>8</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_9"}}" @if ($u->pivot->nota == 9) selected @endif>9</option>  
-                                                        <option value="{{$a->id . "_" . $u->id . "_10"}}" @if ($u->pivot->nota == 10) selected @endif>10</option>
-                                                    </select>
-                                                @else
-                                                    {{$u->pivot->nota}}
-                                                @endif
-                                            </td>
-                                        @endif
-                                    @endforeach
-                                </tr>
-                            @endif
-                        @endforeach
                     </table>
                 </div>
               </div>
