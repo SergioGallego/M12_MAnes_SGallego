e0a4291d0a9eab9f5863c9d6ffd2d8b8e6b6b3bf
Sergio <sergiogallego676@gmail.com>
2022-05-23 17:43:48 +0200

    aprobados suspendidos por modulo sgallego

--- SergioMaximoApp/resources/views/ciclo/stats.blade.php
+++ SergioMaximoApp/resources/views/ciclo/stats.blade.php
@@ -12,13 +12,21 @@
             </div>
         </div>
     </div>
+    @php
+        $statsAlumno = array(array());
+    @endphp
     @foreach($arrayModulos as $key => $m)
+        @php
+            $contadorAlumnos = 0;
+            $aprobadosTotal = 0;
+            $suspendidosTotal = 0;
+        @endphp
         <div class="accordion" id="accordionExample">
             <div class="card">
               <div class="card-header" id="headingOne">
                 <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#{{$m->id}}" aria-expanded="true" aria-controls="collapseOne">
-                    <div style="text-align: left">{{$m->nombre}}</div><div style="text-align: right; text-decoration: none">{{$m->nombre}}</div>
+                  <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#{{$m->id}}" aria-expanded="true" aria-controls="collapseOne">
+                    <div style="text-align: left">{{$m->nombre}}</div>
                   </button>
                 </h2>
               </div>
@@ -48,20 +56,32 @@
                                     @php 
                                         $totalActual = 0;
                                         $aprobadasAlumno = 0;
+                                        $contadorAlumnos = $contadorAlumnos + 1;
+
                                     @endphp
                                     @foreach($a->ufs as $u)
                                         @if($u->modulo_id == $m->id)
                                             @php 
-                                                if(is_null($u->pivot->nota) == false)
+                                                if(is_null($u->pivot->nota) == false){
                                                     $totalActual = $totalActual + 1;
-                                                if($u->pivot->nota >= 5)
+                                                    
+                                                }
+                                                if($u->pivot->nota >= 5){
                                                     $aprobadasAlumno = $aprobadasAlumno + 1;
+                                                }
                                             @endphp
                                             <td> 
                                                 {{$u->pivot->nota}}
                                             </td>
                                         @endif
                                     @endforeach
+                                    @php
+                                        if (@number_format(($aprobadasAlumno * 100 / $contadorUfs)) >= 50) {
+                                            $aprobadosTotal = $aprobadosTotal + 1;
+                                        } else{
+                                            $suspendidosTotal = $suspendidosTotal + 1;
+                                        }
+                                    @endphp
                                     <td> 
                                         @php 
                                             echo @number_format(($aprobadasAlumno * 100 / $totalActual)) . "%";
@@ -78,6 +98,7 @@
                     </table>
                 </div>
               </div>
+              <div style="text-align: right">@php echo "<b>Aprobados:</b> " . @number_format($aprobadosTotal * 100 / $contadorAlumnos) . "% <b>Suspendidos:</b> " . @number_format(($suspendidosTotal * 100 / $contadorAlumnos)) . "%" ; @endphp</div>
             </div>
           </div>
     @endforeach
