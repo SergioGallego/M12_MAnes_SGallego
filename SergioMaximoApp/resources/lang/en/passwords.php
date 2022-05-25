64a3765b94d70c7d60605202fd2445123929dde4
Sergio <sergiogallego676@gmail.com>
2022-05-25 17:01:12 +0200

    reset password sgallego

--- SergioMaximoApp/resources/lang/en/passwords.php
+++ SergioMaximoApp/resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Tu contraseña ha sido reiniciada!',
+    'sent' => 'Hemos enviado un enlace para reiniciar la contraseña!',
+    'throttled' => 'Por favor, espere antes de intentarlo de nuevo.',
+    'token' => 'El token de reinicio de contraseña no es válido.',
+    'user' => "No podemos encontrar a un usuario con ese correo.",
 
 ];
--- SergioMaximoApp/resources/views/auth/forgot-password.blade.php
+++ SergioMaximoApp/resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,38 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.master')
+@section('title')
+    <h1 style="text-align: center"><b>Registro</b></h1>
+@stop
+@section('content')
+<div style="margin-left: auto; margin-right: auto; display: block">
+    <x-guest-layout>
+            <img class="mb-3" src='https://raw.githubusercontent.com/SergioGallego/M12_MAnes_SGallego/main/cropped-logo_calafell_favorit_icon_3.png' style="margin-right: auto; margin-left: auto; display: block">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <x-jet-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('Olvidaste tu contraseña? Sin problemas. Introduce tu correo electrónico y te enviaremos un email con un enlace que te permitirá escoger una nueva.') }}
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <x-jet-validation-errors class="mb-4" />
+
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="block">
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-jet-button style="background-color: rgb(255,103,1); color: white">
+                        {{ __('Reiniciar contraseña') }}
+                    </x-jet-button>
+                </div>
+            </form>
+    </x-guest-layout>
+</div>
+@stop
